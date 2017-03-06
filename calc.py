e4681e3f63d144c3c7c8e241f8ec4bf8abaaa707
svatantra <svatantra.liberte@gmail.com>
2017-03-06 20:03:07 +0530

    update

--- calc.py
+++ calc.py
@@ -1,4 +1,4 @@
-PLUS, MINUS, MUL, DIV = ('+', '-', '*', '/')
+PLUS, MINUS, MUL, DIV ,EOF = ('+', '-', '*', '/','EOF')
 OPERATORS = [PLUS, MINUS, MUL, DIV]
 
 
@@ -21,6 +21,11 @@ class Lexer:
         self.ch = self.text[0]
 
     def advance(self):
+        """
+        Advance in text stream as long as there
+        are characters
+        :return:
+        """
         if self.pos < len(self.text) - 1:
             self.pos += 1
             self.ch = self.text[self.pos]
@@ -28,12 +33,16 @@ class Lexer:
             self.ch = None
 
     def skip_whitespace(self):
-        while self.ch != None and self.ch.isspace():
+        while self.ch is not None and self.ch.isspace():
             self.advance()
 
     def integer(self):
+        """
+        Keep advancing till there are digits in the stream
+        :return:
+        """
         num = ''
-        while self.ch != None and self.ch.isdigit():
+        while self.ch is not None and self.ch.isdigit():
             num += self.ch
             self.advance()
         return int(num)
@@ -43,32 +52,82 @@ class Lexer:
 
     def next(self):
         tok = self.get_next_token()
-        if tok == None:
+        if tok is None:
             raise StopIteration()
         else:
             return tok
 
     def get_next_token(self):
-        while self.ch != None:
+        while self.ch is not None:
             if self.ch.isdigit():
                 return Token(self.integer(), 'INT')
             if self.ch.isspace():
                 self.skip_whitespace()
                 continue
             if self.ch in OPERATORS:
-                tok = Token(self.ch, 'OPERATOR')
+                tok = Token(self.ch, self.ch)
                 self.advance()
                 return tok
+            raise Exception('Invalid Character')
+        return Token(EOF,None)
 
     def print_data(self):
-        while self.ch != None:
+        while self.ch is not None:
             print self.ch
             self.advance()
 
+"""
+   expr::= fact((PLUS|MINUS)fact)*
+   fact::= term((MUL|DIV)term)*
+   term::= INTEGER
+"""
+class Intepreter:
+    def __init__(self,text):
+        self.lex = Lexer(text)
+        self.current_token = self.lex.get_next_token()
+
+    def raise_error(self):
+        raise Exception('Invalid syntax')
+
+    def term(self):
+        token = self.current_token
+        self.consume('INT')
+        return token.data
+
+    def consume(self,type):
+        if self.current_token.type==type:
+            self.current_token = self.lex.get_next_token()
+        else:
+            self.raise_error()
+
+    def fact(self):
+        result = self.term()
+        while self.current_token.type in [DIV,MUL]:
+            if self.current_token.data == DIV:
+                self.consume(DIV)
+                result/=self.term()
+            elif self.current_token.data == MUL:
+                self.consume(MUL)
+                result*=self.term()
+
+        return result
+    def expr(self):
+        result = self.fact()
+        while self.current_token.type in [PLUS,MINUS]:
+            if self.current_token.data == PLUS:
+                self.consume(PLUS)
+                result+=self.fact()
+            elif self.current_token.data == MINUS:
+                self.consume(MINUS)
+                result-=self.fact()
+
+        return result
+
 
 if __name__ == '__main__':
-    lex = Lexer(' 34 + 45 -87 -67')
-    for tok in lex:
-        print tok
 
-    print lex.get_next_token()
+    while True:
+        text = raw_input('cal> ')
+        intrp = Intepreter(text)
+
+        print intrp.expr()
